4eef216a85fb8879f92fe42f4ec1b202d74bfa68
MilenaMarlim <milenamarlim.dev@gmail.com>
2022-08-24 15:10:41 -0300

    Commit com a construção do footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Header from "./components/Header";
 import QuemSomos from "./components/About";
 import GlobalStyle from "./globalStyles";
-import Jobs from "./components/Jobs";
+// import Jobs from "./components/Jobs";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <GlobalStyle />
       <Header />
       <QuemSomos />
-      <Jobs />
+      {/* <Jobs /> */}
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import '../styles/Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="message_footer">
+            <p>Todos os direitos reservados a HKM Developer</p>
+        </div>
+    )
+}
+
+export default Footer
