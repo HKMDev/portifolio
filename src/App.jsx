c7c215cc063a6a5e05c3481313c331c4810dd741
MilenaMarlim <milenamarlim.dev@gmail.com>
2022-08-24 15:27:25 -0300

    Commit com modificação do nome do componente para inglês para uniformizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import QuemSomos from "./components/About";
+import Team from "./components/About";
 import GlobalStyle from "./globalStyles";
 // import Jobs from "./components/Jobs";
 import Footer from "./components/Footer";
@@ -10,7 +10,7 @@ const App = () => {
     <>
       <GlobalStyle />
       <Header />
-      <QuemSomos />
+      <Team />
       {/* <Jobs /> */}
       <Footer />
     </>
