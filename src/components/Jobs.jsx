ea575da2faf513669a372bdb64c046f2868113cd
Keitey <hkmdev22@gmail.com>
2022-08-24 11:10:23 -0300

    alterado compoent de jobs

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,42 +1,104 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
-import * as C from '../styles/Jobs'
+import { Carousel, Card } from "react-bootstrap";
+import * as C from "../styles/Jobs";
 
 import iuhhu from "../img/iuhuu.png";
 import bluemed from "../img/bluemed.png";
-import titan from '../img/titan.png'
-
-
+import titan from "../img/titan.png";
+import agency from "../img/agency.png";
+import agencia from "../img/agencia.png";
+import cafe from "../img/cafe.png";
+import carflix from "../img/carflix.png";
+import electrum from "../img/electrum.png";
 
 function Jobs() {
   return (
     <C.JobContainer>
+      <Card.Text>Conheça um pouco mais do nosso trabalho</Card.Text>
       <Carousel fade>
         <Carousel.Item>
-          <img className="d-block w-100" src={iuhhu} alt="First slide"/>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+          <a
+            href="https://iuhuu.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={iuhhu} alt="iuhhu" />
+          </a>
         </Carousel.Item>
-        <Carousel.Item>
-        <img className="d-block w-100" src={bluemed} alt="First slide"/>
 
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
         <Carousel.Item>
-        <img className="d-block w-100" src={titan} alt="First slide"/>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
+          <a
+            href="https://bluemedconventioncenter.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={titan} alt="titan" />
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a
+            href="https://www.titanlat.com/site/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={bluemed} alt="bluemed" />
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a
+            href="https://agency-react-project.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={agency} alt="agency" />
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a
+            href="https://keitey.github.io/electrum-sass/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={electrum} alt="electrum" />
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a
+            href="https://milenamarlim.github.io/Landing-Page-Cafe/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={cafe} alt="cafe" />
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a
+            href="https://milenamarlim.github.io/landing-page-agencia/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={agencia} alt="agencia" />
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a
+            href="https://milenamarlim.github.io/carfix_landingpage/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={carflix} alt="carflix" />
+          </a>
         </Carousel.Item>
       </Carousel>
+      <Card.Text>
+        *alguns projetos contaram apenas com nossa participação
+      </Card.Text>
     </C.JobContainer>
   );
 }
--- src/styles/Jobs.js
+++ src/styles/Jobs.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const JobContainer = styled.div`
-    width: 90%;
     display: flex;
     justify-content: center;
     align-items: center;
     margin: 0 auto;
+    flex-direction: column;
 `;
