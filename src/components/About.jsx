9f283a71d999d029878e4552b69f01d005d51905
MilenaMarlim <milenamarlim.dev@gmail.com>
2022-08-24 14:04:38 -0300

    Commit com a construção da descrição da empresa

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,9 @@ const About = () => {
       <div>
         <h1>HKM Developer</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id
-          mollis libero. Ut dignissim, augue aliquet imperdiet facilisis, quam
-          augue pulvinar lacus, a pellentesque justo justo vel arcu. Maecenas
-          pharetra dolor eros, et convallis lectus hendrerit ut. Sed fermentum
-          eu turpis in tempus. Proin vehicula consectetur eros quis pulvinar. In
-          interdum arcu in blandit posuere. Fusce sem neque, scelerisque eu
-          interdum ut, tempor at massa. Sed vitae porttitor orci. Sed eu turpis
-          nibh. Etiam nec eros vulputate, placerat enim a.
+        Somos a HKM Developer, equipe especialista em soluções Web!
+          Alguns de nossos serviços incluem construção, desenvolvimento e manutenção de sites, linktrees criativos e inteligentes e construção de materiais para sua rede social!
+          Conheça abaixo o time HKM!
         </p>
       </div>
       <div>
