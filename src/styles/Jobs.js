26f6cab1bb1ec093ce3a6e5d3e0b473abc5a94f8
Keitey <hkmdev22@gmail.com>
2022-08-25 19:58:21 -0300

    modificada cor botões de passagem carousel

--- src/styles/Jobs.js
+++ src/styles/Jobs.js
@@ -1,24 +1,30 @@
 import styled from "styled-components";
 
 export const JobContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-    flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  flex-direction: column;
 
-    h6{
-        font-style: italic;
-        font-size: 10px;
-    }
+  h6 {
+    font-style: italic;
+    font-size: 10px;
+  }
 
-    .carousel-control-next-icon, .carousel-control-prev-icon{
-        background-image: none;
+  .carousel-control-next-icon,
+  .carousel-control-prev-icon {
+    background-image: none;
+  }
+  .card-text {
+    margin-left: auto;
+    font-size: 10px;
+    font-style: italic;
+  }
+
+  .carousel-indicators {
+    button {
+      background-color: #FF8C32;
     }
-    .card-text{
-        margin-left: auto;
-        font-size:10px;
-        font-style:italic;
-       
-    }
+  }
 `;
