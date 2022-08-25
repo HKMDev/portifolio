1bb8dda661eccba3969ecef3494c7ba5aba4ac3c
Keitey <hkmdev22@gmail.com>
2022-08-25 10:39:30 -0300

    add

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,40 +1,15 @@
-// import React from "react";
-// import { Carousel, Card } from "react-bootstrap";
-// import * as C from "../styles/Jobs";
+import React from "react";
+import { Carousel, Card } from "react-bootstrap";
+import * as C from "../styles/Jobs";
 
-<<<<<<< HEAD
-// import iuhhu from "../img/iuhuu.png";
-// import bluemed from "../img/bluemed.png";
-// import titan from "../img/titan.png";
-// import agency from "../img/agency.png";
-// import agencia from "../img/agencia.png";
-// import cafe from "../img/cafe.png";
-// import carflix from "../img/carflix.png";
-// import electrum from "../img/electrum.png";
-
-// function Jobs() {
-//   return (
-//     <C.JobContainer>
-//       <Card.Text>Conheça um pouco mais do nosso trabalho</Card.Text>
-//       <Carousel fade>
-//         <Carousel.Item>
-//           <a
-//             href="https://iuhuu.com.br/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <img className="d-block w-100" src={iuhhu} alt="iuhhu" />
-//           </a>
-//         </Carousel.Item>
-=======
-import iuhhu from "../img/iuhuu.jpg";
-import bluemed from "../img/bluemed.jpg";
-import titan from "../img/titan.jpg";
-import agency from "../img/agency.jpg";
-import agencia from "../img/agencia.jpg";
-import cafe from "../img/cafe.jpg";
-import carflix from "../img/carflix.jpg";
-import electrum from "../img/electrum.jpg";
+import iuhhu from "../img/iuhuu.png";
+import bluemed from "../img/bluemed.png";
+import titan from "../img/titan.png";
+import agency from "../img/agency.png";
+import agencia from "../img/agencia.png";
+import cafe from "../img/cafe.png";
+import carflix from "../img/carflix.png";
+import electrum from "../img/electrum.png";
 
 function Jobs() {
   return (
@@ -52,86 +27,66 @@ function Jobs() {
             <img className="d-block w-100" src={iuhhu} alt="iuhhu" />
           </a>
         </Carousel.Item>
->>>>>>> 88c3a674c5023d705c67ef395c3e444fabdeeca4
+     <Carousel.Item>
+         <a
+            href="https://bluemedconventioncenter.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={titan} alt="titan" />
+          </a>
+        </Carousel.Item>
 
-//         <Carousel.Item>
-//           <a
-//             href="https://bluemedconventioncenter.com.br/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <img className="d-block w-100" src={titan} alt="titan" />
-//           </a>
-//         </Carousel.Item>
+        <Carousel.Item>
+          <a
+            href="https://www.titanlat.com/site/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={bluemed} alt="bluemed" />
+          </a>
+        </Carousel.Item>
 
-//         <Carousel.Item>
-//           <a
-//             href="https://www.titanlat.com/site/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <img className="d-block w-100" src={bluemed} alt="bluemed" />
-//           </a>
-//         </Carousel.Item>
+        <Carousel.Item>
+          <a
+            href="https://agency-react-project.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={agency} alt="agency" />
+          </a>
+        </Carousel.Item>
 
-//         <Carousel.Item>
-//           <a
-//             href="https://agency-react-project.netlify.app/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <img className="d-block w-100" src={agency} alt="agency" />
-//           </a>
-//         </Carousel.Item>
+        <Carousel.Item>
+          <a
+            href="https://keitey.github.io/electrum-sass/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={electrum} alt="electrum" />
+          </a>
+        </Carousel.Item>
 
-//         <Carousel.Item>
-//           <a
-//             href="https://keitey.github.io/electrum-sass/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <img className="d-block w-100" src={electrum} alt="electrum" />
-//           </a>
-//         </Carousel.Item>
+        <Carousel.Item>
+          <a
+            href="https://milenamarlim.github.io/Landing-Page-Cafe/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={cafe} alt="cafe" />
+          </a>
+        </Carousel.Item>
 
-//         <Carousel.Item>
-//           <a
-//             href="https://milenamarlim.github.io/Landing-Page-Cafe/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <img className="d-block w-100" src={cafe} alt="cafe" />
-//           </a>
-//         </Carousel.Item>
+        <Carousel.Item>
+          <a
+            href="https://milenamarlim.github.io/landing-page-agencia/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="d-block w-100" src={agencia} alt="agencia" />
+          </a>
+        </Carousel.Item>
 
-//         <Carousel.Item>
-//           <a
-//             href="https://milenamarlim.github.io/landing-page-agencia/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <img className="d-block w-100" src={agencia} alt="agencia" />
-//           </a>
-//         </Carousel.Item>
-
-<<<<<<< HEAD
-//         <Carousel.Item>
-//           <a
-//             href="https://milenamarlim.github.io/carfix_landingpage/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <img className="d-block w-100" src={carflix} alt="carflix" />
-//           </a>
-//         </Carousel.Item>
-//       </Carousel>
-//       <Card.Text>
-//         *alguns projetos contaram apenas com nossa participação
-//       </Card.Text>
-//     </C.JobContainer>
-//   );
-// }
-=======
         <Carousel.Item>
           <a
             href="https://milenamarlim.github.io/carfix_landingpage/"
@@ -143,11 +98,11 @@ function Jobs() {
         </Carousel.Item>
       </Carousel>
       <Card.Text>
-        <h6>*alguns projetos contaram apenas com nossa participação</h6>
+        *alguns projetos contaram apenas com nossa participação
       </Card.Text>
     </C.JobContainer>
   );
 }
->>>>>>> 88c3a674c5023d705c67ef395c3e444fabdeeca4
 
-// export default Jobs;
+
+export default Jobs;
