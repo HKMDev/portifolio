8dc30482ec7730b4200dd2795bf0f25d974ba4d2
Keitey <hkmdev22@gmail.com>
2022-08-26 08:59:58 -0300

    add gif no component contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import * as C from "../styles/Contact";
 import { MdEmail } from "react-icons/md";
+import robot from "../img/robot.gif";
 
 const Contact = () => {
   return (
     <C.ContactText>
+      <img src={robot} alt="" />
+      <div className="contact">
       Entre em contato conosco
-      <a href="mailto:hkmdev22@gmail.com" target="_blank" rel="noopener noreferrer">
-        <MdEmail size={25}/>
+      <a
+        href="mailto:hkmdev22@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <MdEmail size={32} />
       </a>
+      </div>
     </C.ContactText>
   );
 };
--- src/styles/Contact.js
+++ src/styles/Contact.js
@@ -1,18 +1,43 @@
 import styled from "styled-components";
 
 export const ContactText = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 0.5rem;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    font-weight: bold;
-    font-size: 18px;
-    height: 100px;
-    color: #FF8C32;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 1rem;
+  font-weight: bold;
+  height: 150px;
+  color: #ff8c32;
+  margin-top: 35px;
+  font-size: 22px;
 
-    path{
-        color: #FBA53B;
+  .contact {
+    margin-top: 45px;
+    display: flex;
+    gap: 0.5rem;
+  }
+
+  svg {
+    color: #fba53b;
+  }
+
+  img {
+    height: 98px;
+  }
+
+  @media (max-width: 650px) {
+    flex-direction: column;
+    height: 200px;
+
+    img {
+      height: 60px;
     }
+
+    .contact {
+      margin-top: 20px;
+      flex-direction: column;
+      font-size: 16px;
+    }
+  }
 `;
